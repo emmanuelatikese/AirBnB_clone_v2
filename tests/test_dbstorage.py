842e8c2f147c96f078f33923a962c444f612fbe1
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-20 10:09:37 -0800

    adding

--- tests/test_dbstorage.py
+++ tests/test_dbstorage.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 '''Another comment'''
-import MySQLdb
+import mysql.connector
 import unittest
 from os import getenv
 
 class Test_db(unittest.TestCase):
     """This thing start today"""
     def test_dbs(self):
-        con = MySQLdb.connect(host=getenv("HBNB_MYSQL_HOST"),
+        con = mysql.connector.connect(host=getenv("HBNB_MYSQL_HOST"),
                                           port=3306,
-                                          user=getenv("hbnb_test"),
-                                          passwd=getenv("hbnb_test_pwd"),
-                                          db=getenv("hbnb_test_db"),
+                                          user=getenv("HBNB_MYSQL_USER"),
+                                          passwd=getenv("HBNB_MYSQL_PWD"),
+                                          db=getenv("HBNB_MYSQL_DB"),
                                           charset="utf8")
-        cur = conn.cursor()
+        cur = con.cursor()
         cur.execute("SELECT * FROM states")
         query_row = cur.fetchall();
         cur_state = len(query_row)
 
         cur.execute("INSERT INTO states (name) VALUES ('California')")
+        con.commit()
+
+        cur.execute("SELECT * FROM states")
         new_query = cur.fetchall();
         new_state = len(new_query)
         cur.close()
-        conn.close()
+        con.close()
         return self.assertEqual(cur_state + 1, new_state)
+if __name__ == "__main__":
+    unittest.main()
