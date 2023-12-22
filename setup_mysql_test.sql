1daef136ac87d83fb09233bc4345296b6338cac8
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-21 17:04:50 -0800

    adding

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- This is privileges we all need
+-- another comment
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
