db5693ee213707aefd5c8787e84c22583097704c
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-21 16:57:37 -0800

    adding

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- This is privileges we all need
 -- another comment
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
