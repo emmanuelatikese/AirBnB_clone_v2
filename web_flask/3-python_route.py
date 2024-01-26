902d829c38caaf28fd01f57c65b1785bea2d2708
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-26 13:14:34 -0800

    adding

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,11 +6,13 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def index(text='is cool'):
     text = text.replace('_', ' ')
     return ('Python %s' % text)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
