19ca8bf1a37f5edb9fedc07759c4bb6d682d1fe9
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-26 03:07:35 -0800

    adding

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,9 +5,11 @@ from flask import Flask
 
 web = Flask(__name__)
 
+
 @web.route('/hbnb', strict_slashes=False)
 def index():
     return ('HBNB')
+
 
 if __name__ == "__main__":
     web.run(host='0.0.0.0', port=5000)
