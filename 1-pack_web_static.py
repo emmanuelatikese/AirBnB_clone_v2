952e6ce95e6be8cdb0b97c2456d8539df3195596
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-28 09:46:02 +0000

    adding

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 '''fabric on the first try'''
 
-import os
-import fabric.api import local
-import datetime import datetime
+from os import path, makedirs
+from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
     try:
-        if not os.path.exists("versions"):
-            os.makedir("versions")
+        if not path.exists("versions"):
+            makedirs("versions")
         arch_dt = datetime.strftime("%Y%m%d%H%M%S")
-        arch_name = f"web_static_{arch_dt}.tgz"
+        arch_name = "web_static_{}.tgz".format(arch_dt)
         local("tar -czvf versions/{} web_static".format(arch_name))
         return "versions/{}".format(arch_name)
     except Exception as e:
